f399395315b47ca83505e589522b2cbda19bd531
fitdailyid <dailyidfit@gmail.com>
2025-12-28 23:41:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function goToShopee() {
+  window.open("https://id.shp.ee/LLzMd6m", "_blank");
+}
+
+// Scroll Reveal
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
